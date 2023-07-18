414517888a17984b03fc229161b76a683db04e79
roj21008 <roj21008@byui.edu>
2023-07-18 15:24:36 -0500

    z

--- _test_/mongo.spec.js
+++ _test_/mongo.spec.js
@@ -16,13 +16,13 @@ describe('insert', () => {
     });
     afterAll(async() => {
         await connection.close()
-    })
+    });
 
     it('should insert a new admin into the admins collection', async () => {
-        const admin = db.collection('admins');
+       const admin = db.collection('admins');
 
         const mockUser = {
-            id: 'admin-id',
+            id: 'new-id',
             user: 'rojas',
             password: "1011267",
             
@@ -30,15 +30,15 @@ describe('insert', () => {
 
         await admin.insertOne(mockUser)
 
-        const insertedUser = await admin.findOne({ id: 'admin-id' });
+        const insertedUser = await admin.findOne({ id: 'new-id' });
 
         expect(insertedUser).toEqual(mockUser)
     },
         
     it('should delete a user from the users collection', async () => {
         const admins = db.collection('admins')
-        await admins.deleteMany({ id: 'admin-id' })
-        const deletedAdmin = await admins.findOne({ id: 'admin-id' });
+        await admins.deleteMany({ id: 'new-id' })
+        const deletedAdmin = await admins.findOne({ id: 'new-id' });
         expect(deletedAdmin).toEqual(null)
     })
 )})
