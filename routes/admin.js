bb4f4772c7a3a795efd973a27cec19ed4ea4374c
Christian <vargasc475@gmail.com>
2023-07-19 01:12:15 -0400

    disabled authentication

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const adminController = require('../controllers/admin');
-const { isAuthenticated } = require('../middleware/authenticate');
+// const { isAuthenticated } = require('../middleware/authenticate');
 
-router.get('/getAdmin', isAuthenticated, adminController.getAlladmins);
-router.post('/newAdmin', isAuthenticated, adminController.newAdmin);
-router.delete('/deleteAdmin/:id', isAuthenticated, adminController.adminDelete);
+router.get('/getAdmin', adminController.getAlladmins);
+router.post('/newAdmin', adminController.newAdmin);
+router.delete('/deleteAdmin/:id', adminController.adminDelete);
 
 
 
--- routes/dishes.js
+++ routes/dishes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const dishController = require('../controllers/dishes');
-const { isAuthenticated } = require('../middleware/authenticate');
+// const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/getDishes', dishController.getAlldishes);
-router.post('/newDish', isAuthenticated, dishController.newDish);
-router.delete('/deleteDish/:id', isAuthenticated, dishController.deleteDish);
+router.post('/newDish', dishController.newDish);
+router.delete('/deleteDish/:id', dishController.deleteDish);
 
 
 
