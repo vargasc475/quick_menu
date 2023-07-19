ee9cb4240f6efba9c9eab3e4451dfb87b70d3da1
Christian <vargasc475@gmail.com>
2023-07-19 00:52:07 -0400

    fixed the routes

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ router.use('/', require('./dessert'));
 router.use('/', require('./admin'));
 router.use('/', require('./order'));
 router.use('/', require('./swaggerRoute'));
-routes.get('/login', passport.authenticate('github'), (req, res) => {});
+router.get('/login', passport.authenticate('github'), (req, res) => {});
 
-routes.get('/logout', function(req, res, next) {
+router.get('/logout', function(req, res, next) {
     req.logout(function(err) {
         if (err) { return next(err); }
         res.redirect('/');
