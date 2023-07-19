f5f2862f815940321212cfaf84ec07422980f100
Christian <vargasc475@gmail.com>
2023-07-19 00:51:32 -0400

    Added routes to login and loggout

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,14 @@ router.use('/', require('./dessert'));
 router.use('/', require('./admin'));
 router.use('/', require('./order'));
 router.use('/', require('./swaggerRoute'));
+routes.get('/login', passport.authenticate('github'), (req, res) => {});
+
+routes.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
 
 module.exports = router;
